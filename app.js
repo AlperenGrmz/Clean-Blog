3b51cbf3085f409d71b80e4ef738d6444029bfe4
Alperen Gormez <alperen.41gs@hotmail.com>
2022-12-31 21:52:12 +0300

    3

--- app.js
+++ app.js
@@ -1,27 +1,48 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path')
 const app = express();
+const Blog = require('./models/Blog')
+
+//connect DB
+
+mongoose.connect('mongodb://127.0.0.1:27017/cleanBlog-test-db');
+
 
 app.set("view engine","ejs");
 
 app.use(express.static("public"))
-
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
 
 
 const port = 3000;
 
-app.get("/",(req,res) => {
-
-    res.render("index")
+app.get("/",async(req,res) => {
+    const blogs = await Blog.find({})
+    res.render("index", {
+        blogs
+    })
 })
 app.get("/about",(req,res) => {
 
     res.render("about")
 })
+
+app.get("/post",(req,res) => {
+
+    res.render("post")
+})
 app.get("/add_post",(req,res) => {
 
     res.render("add_post")
+})
+
+app.post("/blogs",async(req,res) => {
+
+    await Blog.create(req.body);
+    res.redirect('/');
 })
 
 
